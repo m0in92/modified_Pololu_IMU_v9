f766c6020da9a0ecbd52b50eeee60caae985271b
m0in92 <moinahmed100@gmail.com>
2025-07-18 14:32:17 -0400

    update

--- ArduinoIDE/LSM6DS33/example_serial/example_serial.ino
+++ ArduinoIDE/LSM6DS33/example_serial/example_serial.ino
@@ -9,14 +9,21 @@
 PololuIMUv9 imu;
 
 float gyro_roll, gyro_pitch, gyro_yaw;
-float gyro_roll_prev, gyro_pitch_prev, gyro_yaw_prev;
+float accel_roll, accel_pitch;
+float comp_roll, comp_pitch;
 
-float roll, pitch;
+// moving average variables
+const int SIZE = 10;
+float array_accel_roll[SIZE];
+float array_accel_pitch[SIZE];
+
+// complimentary filter parameters
+const float ALPHA = 0.9;
 
 unsigned long time_present, time_prev;
 float dt;
 
-TiltData tilt_data;
+TiltData accel_tilt, gyro_tilt;
 
 void setup() {
   Serial.begin(9600);
@@ -25,45 +32,55 @@ void setup() {
 
   delay(1000);
 
-  imu.read();
-  roll = tilt_from_accelerometer(imu.a.x, imu.a.y, imu.a.z).roll;
-  pitch = tilt_from_accelerometer(imu.a.x, imu.a.y, imu.a.z).pitch;
-  delay(100);
+  accel_tilt = filter::filtered_tilt_accel(&imu, 10);
+  Serial.print(accel_tilt.roll);
+  Serial.print(",");
+  Serial.println(accel_tilt.pitch);
 
   time_prev = micros();
-  gyro_roll_prev = roll;
-  gyro_pitch_prev = pitch;
+
+  gyro_roll = accel_roll;
+  gyro_pitch = accel_pitch;
 }
 
 int value, temp;
 void loop() {
-  imu.read();
-
-  delay(100);
-
-  float roll = tilt_from_accelerometer(imu.a.x, imu.a.y, imu.a.z).roll;
-  float pitch = tilt_from_accelerometer(imu.a.x, imu.a.y, imu.a.z).pitch;
-
-  time_present = micros();
-  dt = (time_present - time_prev) / (float) 1e6;  // [s]
-
-  gyro_roll = gyro_roll_prev + imu.g.x * dt;
-  gyro_pitch = gyro_pitch_prev + imu.g.y * dt;
-
-  Serial.print(roll);
+  accel_tilt = filter::filtered_tilt_accel(&imu, 1);
+  Serial.print(accel_tilt.roll);
   Serial.print(",");
-  Serial.print(pitch);
-  Serial.print(",");
+  Serial.println(accel_tilt.pitch);
+  // imu.read();
+  // delay(100);
 
-  Serial.print(gyro_roll);
-  Serial.print(",");
-  Serial.println(gyro_pitch);
+  // accel_roll = tilt_from_accelerometer(&imu).roll;
+  // accel_pitch = tilt_from_accelerometer(&imu).pitch;
 
-  // update variables
-  gyro_roll_prev = gyro_roll;
-  gyro_pitch_prev = gyro_pitch;
+  // time_present = micros();
+  // dt = (time_present - time_prev) / (float) 1e6;  // [s]
 
-  time_prev = time_present;
+  // gyro_roll = gyro_roll + (-imu.g.y) * dt;
+  // gyro_pitch = gyro_pitch + imu.g.x * dt;
+
+  // comp_roll = 0.5 * accel_roll + 0.5 * gyro_roll;
+  // comp_pitch = 0.5 * accel_pitch + 0.5 * gyro_pitch;
+
+  // Serial.print(accel_roll);
+  // Serial.print(",");
+  // Serial.println(accel_pitch);
+  // Serial.print(",");
+
+  // Serial.print(gyro_roll);
+  // Serial.print(",");
+  // Serial.print(gyro_pitch);
+  // Serial.print(",");
+
+  // Serial.print(comp_roll);
+  // Serial.print(",");
+  // Serial.println(comp_pitch);
+
+  // update variable
+  // time_prev = time_present;
+
   // Serial.print(imu.g.x);
   // Serial.print(",");
   // Serial.print(imu.g.y);
@@ -77,5 +94,5 @@ void loop() {
   // Serial.print(",");
   // Serial.println(imu.m.z);
 
-  delay(100);
+  // delay(100);
 }
--- ArduinoIDE/LSM6DS33/example_serial/tilt.h
+++ ArduinoIDE/LSM6DS33/example_serial/tilt.h
@@ -1,7 +1,10 @@
 #ifndef TILT_H
 #define TILT_H
 
+#include <Arduino.h>
 #include <math.h>
+
+#include "PololuIMUv9.h"
 
 // constants declarations
 // const float PI = 3.14159;
@@ -23,13 +26,51 @@ struct TiltData
  * @param a_z
  * @return TiltData
  */
-TiltData tilt_from_accelerometer(float a_x, float a_y, float a_z)
+TiltData tilt_from_accelerometer(PololuIMUv9 *imu)
 {
     TiltData data;
-    data.roll = atan2(a_x, a_z) * TO_DEG;
-    data.pitch = atan2(a_y, a_z) * TO_DEG;
+    data.roll = atan2(imu->a.x, imu->a.z) * TO_DEG;
+    data.pitch = atan2(imu->a.y, imu->a.z) * TO_DEG;
 
     return data;
 }
+
+namespace filter
+{
+    float _calc_avg(float *ptr_data_array, int SIZE)
+    {
+        float sum = 0.0;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            sum += *(ptr_data_array + i);
+        }
+        return sum / (float)SIZE;
+    }
+
+    TiltData filtered_tilt_accel(PololuIMUv9 *imu, int SIZE)
+    {
+        float sum_roll = 0;
+        float sum_pitch = 0;
+        TiltData data;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            imu->read();
+            data = tilt_from_accelerometer(imu);
+
+            sum_roll += data.roll;
+            sum_pitch += data.pitch;
+
+            delay(100);
+        }
+
+        TiltData result;
+        result.roll = sum_roll / (float)SIZE;
+        result.pitch = sum_pitch / (float)SIZE;
+
+        return result;
+    }
+};
 
 #endif // TILT_H
